70a74092bb5bc497bdc483ba17e0ffcdd41f95ad
atymous <atymous@ucmerced.edu>
2020-10-25 18:10:53 -0700

    use addEventListener to perform click

--- performAction.js
+++ performAction.js
@@ -1,1 +1,12 @@
 console.log("working!")
+
+/* We want to query the items, which are the theme dot buttons */
+let colorThemeDots = document.getElementsByClassName('color-theme-dot')
+
+for (var i=0; colorThemeDots.length > i; i++){
+  /* for each iteration we want the item to get stored into the array  
+  --> use 'addeventListener()' function */
+  colorThemeDots[i].addEventListener('click', function(){
+    console.log('Theme Color Selected')
+  })
+}
