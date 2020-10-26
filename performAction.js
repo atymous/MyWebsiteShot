3f751fe89d919f19ce1ea640723dedbb7222d78e
atymous <atymous@ucmerced.edu>
2020-10-25 18:16:27 -0700

    add custom attributes so we know what btn we click

--- performAction.js
+++ performAction.js
@@ -7,6 +7,9 @@ for (var i=0; colorThemeDots.length > i; i++){
   /* for each iteration we want the item to get stored into the array  
   --> use 'addeventListener()' function */
   colorThemeDots[i].addEventListener('click', function(){
-    console.log('Theme Color Selected')
+    /* query this out so we can actually select correct theme color
+    --> take 'mode' and console it out */
+    let mode = this.dataset.mode
+    console.log('Theme Color Selected: ', mode)
   })
 }
